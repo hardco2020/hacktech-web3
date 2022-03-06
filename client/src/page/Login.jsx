780b47cd9c1569504d09252025cc983b22346df1
Kai <hardco2020@gmail.com>
2022-03-05 17:57:05 -0800

    add: register and login UI

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
-
-const Login = () => {
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Img,
+  Center
+} from '@chakra-ui/react';
+import logo from '../assets/logo.png';
+import { FcGoogle } from 'react-icons/fc';
+import { Link as Route } from 'react-router-dom';
+export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Img src={logo} boxSize="60%"/>
+          <Heading fontSize={'4xl'}></Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            sign in to enjoy all the cool <Link color={'blue.400'}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <Button w={'full'} variant={'outline'} leftIcon={<FcGoogle />}>
+              <Center>
+                <Text>Sign in with Google</Text>
+              </Center>
+            </Button>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.400'}>Forgot password?</Link>
+                <Route to="/register">
+                  <Link color={'blue.400'}>Sign up</Link>
+                </Route>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 }
-
-export default Login
